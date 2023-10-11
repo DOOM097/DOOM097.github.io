5772342a6621aed12649c6dafe8a4ad398fb0470
DOOM097 <98549250+DOOM097@users.noreply.github.com>
2023-10-11 15:07:25 +0300

    Add files via upload

---
+++ react-twitter/precache-manifest.5dd5951ce663f37b080dfd830a55602a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8e28d9042ef8895d975",
+    "url": "/react-twitter/static/css/main.8afb2f9f.chunk.css"
+  },
+  {
+    "revision": "c8e28d9042ef8895d975",
+    "url": "/react-twitter/static/js/main.c8e28d90.chunk.js"
+  },
+  {
+    "revision": "512a9bcb7bed75ea6d8e",
+    "url": "/react-twitter/static/css/1.4d91427e.chunk.css"
+  },
+  {
+    "revision": "512a9bcb7bed75ea6d8e",
+    "url": "/react-twitter/static/js/1.512a9bcb.chunk.js"
+  },
+  {
+    "revision": "345c218d77bec60fae51",
+    "url": "/react-twitter/static/js/runtime~main.345c218d.js"
+  },
+  {
+    "revision": "078ea073039bcfe916171a891fa4b427",
+    "url": "/react-twitter/index.html"
+  }
+];
