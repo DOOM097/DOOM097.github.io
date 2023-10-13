85c479a892cf3642c8c6add79159c7d3482e2f48
DOOM097 <98549250+DOOM097@users.noreply.github.com>
2023-10-13 14:08:57 +0300

    Add files via upload

---
+++ iceandfire-react/precache-manifest.26fb3582d89416b1239f4f322b679d7f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "03b1d3208d62efb5378d",
+    "url": "/iceandfire-react/static/css/main.eab83792.chunk.css"
+  },
+  {
+    "revision": "03b1d3208d62efb5378d",
+    "url": "/iceandfire-react/static/js/main.03b1d320.chunk.js"
+  },
+  {
+    "revision": "4fae826bca2fe8afc74a",
+    "url": "/iceandfire-react/static/css/1.3518352c.chunk.css"
+  },
+  {
+    "revision": "4fae826bca2fe8afc74a",
+    "url": "/iceandfire-react/static/js/1.4fae826b.chunk.js"
+  },
+  {
+    "revision": "5ca1e2482fa5f340ba79",
+    "url": "/iceandfire-react/static/js/runtime~main.5ca1e248.js"
+  },
+  {
+    "revision": "22389cf47ad284464c13506f2e22e8af",
+    "url": "/iceandfire-react/static/media/error.22389cf4.jpg"
+  },
+  {
+    "revision": "4da810837c6dd45262462cccf9566c34",
+    "url": "/iceandfire-react/static/media/got.4da81083.jpeg"
+  },
+  {
+    "revision": "77549c3401ef0225de78b0796db27604",
+    "url": "/iceandfire-react/index.html"
+  }
+];
